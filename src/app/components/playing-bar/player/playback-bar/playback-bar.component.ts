67d26194271c9c0b3f03b5f1aba7aa79d1aa7d7d
Miguel Muñoz Caro <miguelmc.dev@gmail.com>
2024-02-27 13:52:25 -0300

    (feat) progress bar component - visual

--- src/app/components/playing-bar/player/playback-bar/playback-bar.component.ts
+++ src/app/components/playing-bar/player/playback-bar/playback-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-playback-bar',
   standalone: true,
-  imports: [],
+  imports: [ProgressBarComponent],
   templateUrl: './playback-bar.component.html',
   styleUrl: './playback-bar.component.css'
 })
